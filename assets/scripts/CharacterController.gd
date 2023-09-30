ee495c78bee33ead2c87eb0ec22206d9a189979e
GrmSeven <grmseven7@gmail.com>
2023-09-30 16:25:03 +0300

    sthstusy

--- assets/scripts/CharacterController.gd
+++ assets/scripts/CharacterController.gd
@@ -3,12 +3,17 @@ extends CharacterBody2D
 const accel: int = 30
 const max_speed: int = 150
 const sprint_mult: float = 1.5
+var sprint
+var direction
+var facing
 
 func _process(delta):
-	var sprint = Input.is_key_pressed(KEY_SHIFT)
-	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	sprint = Input.is_key_pressed(KEY_SHIFT)
+	direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	if direction:
 		velocity = velocity.move_toward(direction * max_speed * (sprint_mult if sprint else 1), accel)
+		facing = direction.ceil()
+		$Area2D.position = facing * 15
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, accel)
 	move_and_slide()
