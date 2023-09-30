37e2492f12370f7fba473fa00a8261a774f499a3
GrmSeven <grmseven7@gmail.com>
2023-09-30 21:07:46 +0300

    workin

--- assets/scripts/CharacterController.gd
+++ assets/scripts/CharacterController.gd
@@ -2,16 +2,13 @@ extends CharacterBody2D
 
 const accel: int = 30
 const max_speed: int = 150
-const sprint_mult: float = 1.5
-var sprint
 var direction
 var facing
 
 func _process(_delta):
-	sprint = Input.is_key_pressed(KEY_SHIFT)
 	direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	if direction:
-		velocity = velocity.move_toward(direction * max_speed * (sprint_mult if sprint else 1), accel)
+		velocity = velocity.move_toward(direction * max_speed, accel)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, accel)
 		
