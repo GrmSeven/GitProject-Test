74a511541d2861055c0ea72995f5b2986947e35a
Kaur Huko Käämbre <105814740+KaurHuko@users.noreply.github.com>
2023-09-30 20:09:03 +0300

    q

--- assets/scripts/CharacterController.gd
+++ assets/scripts/CharacterController.gd
@@ -7,19 +7,12 @@ var sprint
 var direction
 var facing
 
-func _process(_delta):
+func _process(delta):
 	sprint = Input.is_key_pressed(KEY_SHIFT)
 	direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	if direction:
 		velocity = velocity.move_toward(direction * max_speed * (sprint_mult if sprint else 1), accel)
+		facing = direction.ceil()
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, accel)
-		
-	if direction == Vector2.ZERO:
-		$AnimationTree.get("parameters/playback").travel("Idle")
-	else:
-		$AnimationTree.get("parameters/playback").travel("Walk")
-		$AnimationTree.set("parameters/Idle/blend_position", velocity)	
-		$AnimationTree.set("parameters/Walk/blend_position", velocity)
-		
-	move_and_slide() 
+	move_and_slide()
