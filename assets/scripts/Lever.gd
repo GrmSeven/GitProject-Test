c685fc9ca0659bbde1e5059e1787ae56d35a83e4
Kaur Huko Käämbre <105814740+KaurHuko@users.noreply.github.com>
2023-09-30 21:50:48 +0300

    a

--- assets/scripts/Lever.gd
+++ assets/scripts/Lever.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+var AA: String = "UHH"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +10,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_body_entered(body: Node2D):
+	if body.is_in_group("PlayerInteraction"):
+		print(AA)
