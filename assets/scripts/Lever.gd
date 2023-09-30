91befa6f0b14a90a1090dedb0f33170c37ba0c3e
Kaur Huko Käämbre <105814740+KaurHuko@users.noreply.github.com>
2023-10-01 02:02:50 +0300

    a

--- assets/scripts/Lever.gd
+++ assets/scripts/Lever.gd
@@ -1,23 +1,26 @@
-extends Node2D
+extends Area2D
 
 var AA: String = "UHH"
-@export var door: Node2D
+
+@export var disabled_sprite: Sprite2D
+@export var enabled_sprite: Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
 func on_player_interact():
 	print(AA)
-
+	
 func can_interact():
 	pass
-
+	
 func cannot_interact():
 	pass
+
+func get_door():
+	return get_owner().door
