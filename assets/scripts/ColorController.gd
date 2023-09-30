96c86f3f1ac20a48a86a6a4c04edfae9070d2b90
GrmSeven <grmseven7@gmail.com>
2023-09-30 10:50:52 +0300

    colors are done

--- assets/scripts/ColorController.gd
+++ assets/scripts/ColorController.gd
@@ -5,9 +5,9 @@ var speed = 10
 var current = colors[mode]
 
 func _ready():
-	color = Color(current, 1)
+	color = Color(current[0], current[1], current[2], 1)
 
 func _process(delta):
 	mode = 0 if Input.is_key_label_pressed(KEY_1) else (1 if Input.is_key_label_pressed(KEY_2) else (2 if Input.is_key_label_pressed(KEY_3) else mode))
-	
-	color = Color(current.move_toward(colors[mode], speed), 1)
+	current = current.move_toward(colors[mode], speed * delta)
+	color = Color(current[0], current[1], current[2], 1)
