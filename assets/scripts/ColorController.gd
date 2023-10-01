813fd5af3467c6b474846caf2a34d8b64f91949b
Kaur Huko Käämbre <105814740+KaurHuko@users.noreply.github.com>
2023-10-01 15:33:37 +0300

    a

--- assets/scripts/ColorController.gd
+++ assets/scripts/ColorController.gd
@@ -4,7 +4,9 @@ var colors = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
 var tween_active = false
 
 func _ready():
-	color = Color(colors[global.color_mode][0], colors[global.color_mode][1], colors[global.color_mode][2], 1)
+
+	if !(get_tree().current_scene.scene_file_path == "res://assets/scenes/Cutscene.tscn"):
+		color = Color(colors[global.color_mode][0], colors[global.color_mode][1], colors[global.color_mode][2], 1)
 	global.color_controller = self
 
 func _process(_delta):
