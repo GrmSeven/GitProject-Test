b82b7e0acc244e9dda9c6c880f8e618e800bcf1c
GrmSeven <grmseven7@gmail.com>
2023-10-01 12:03:13 +0300

    n ou

--- assets/scripts/CutsceneController.gd
+++ assets/scripts/CutsceneController.gd
@@ -22,6 +22,7 @@ func _process(_delta):
 				i.visible = false
 			tween.kill()
 			tween2 = create_tween()
+			tween2.tween_property($"CanvasModulate", "color", Color(0.2, 0.2, 0.2, 1), 0)
 			tween2.tween_property($ColorRect2, "modulate", Color(1, 1, 1, 0), 2).set_ease(Tween.EASE_OUT_IN)
 			tween2.tween_property(self, "done", true, 0)
 			tween2.connect("finished", on_tween2_finished)	
@@ -31,8 +32,9 @@ func on_tween2_finished():
 	tween2 = create_tween()
 	tween2.tween_property($CharacterBody2D, "position", Vector2(0, -300), 4).set_ease(Tween.EASE_IN)
 	tween2.tween_property($Artifact, "visible", false, 1)
-	tween2.tween_property($"/root/CanvasModulate", "color", Color.BLACK, 2)
-	tween2.tween_property(self, "color", Color(1, 0, 0, 1), 0.5)
-	
-
+	tween2.tween_property($"CanvasModulate", "color", Color.BLACK, 2)
+	tween2.tween_property($"CanvasModulate", "color", Color.RED, 2)
+	tween2.tween_property($"CanvasModulate", "color", Color.BLACK, 2)
+	tween2.parallel()
+	tween2.tween_property($CharacterBody2D, "position", Vector2(0, -360), 2).set_ease(Tween.EASE_IN)
 	
