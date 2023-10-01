09b79d9ccaa11ca23ea42f3e6d7b33a4f6b3baab
GrmSeven <grmseven7@gmail.com>
2023-10-01 11:26:06 +0300

    e

--- assets/scripts/CharacterController.gd
+++ assets/scripts/CharacterController.gd
@@ -8,16 +8,18 @@ var prev_pos = Vector2.ZERO
 var actual_velocity = Vector2.ZERO
 
 func _process(_delta):
+	
 	if (global.paused):
 		$AnimationTree.get("parameters/playback").travel("Idle")
 		return
-	if direction == Vector2.ZERO or actual_velocity.length() == 0:
+	if actual_velocity.length() == 0:
 		$AnimationTree.get("parameters/playback").travel("Idle")
 	else:
 		$AnimationTree.get("parameters/playback").travel("Walk")
 		$AnimationTree.set("parameters/Idle/blend_position", velocity)	
 		$AnimationTree.set("parameters/Walk/blend_position", velocity)	
-	direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	if global.movement_on:
+		direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	if direction:
 		velocity = velocity.move_toward(direction * max_speed, accel)
 	else:
@@ -25,5 +27,5 @@ func _process(_delta):
 		
 	actual_velocity = prev_pos - position
 	prev_pos = position
-	
+
 	move_and_slide()
--- assets/scripts/global.gd
+++ assets/scripts/global.gd
@@ -5,3 +5,5 @@ var color_mode = 0
 var paused: bool = false
 
 var color_controller: CanvasModulate
+
+var movement_on = false
