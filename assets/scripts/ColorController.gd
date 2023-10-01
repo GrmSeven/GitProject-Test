13b03341ffffa1bd23d5d652d7b75bfc21f23a56
GrmSeven <grmseven7@gmail.com>
2023-10-01 13:03:35 +0300

    e

--- assets/scripts/ColorController.gd
+++ assets/scripts/ColorController.gd
@@ -8,11 +8,12 @@ func _ready():
 	global.color_controller = self
 
 func _process(_delta):
-	var shift_input = Input.is_action_pressed("shift_color")
-	if shift_input and !tween_active:
-		tween_active = true
-		var tween = create_tween()
-		global.color_mode = fmod((global.color_mode + 1), 3)
-		tween.tween_property(self, "color", Color.BLACK, speed)
-		tween.tween_property(self, "color", Color(colors[global.color_mode][0], colors[global.color_mode][1], colors[global.color_mode][2], 1), speed)
-		tween.tween_property(self, "tween_active", false, 0)
+	if global.movement_on:
+		var shift_input = Input.is_action_pressed("shift_color")
+		if shift_input and !tween_active:
+			tween_active = true
+			var tween = create_tween()
+			global.color_mode = fmod((global.color_mode + 1), 3)
+			tween.tween_property(self, "color", Color.BLACK, speed)
+			tween.tween_property(self, "color", Color(colors[global.color_mode][0], colors[global.color_mode][1], colors[global.color_mode][2], 1), speed)
+			tween.tween_property(self, "tween_active", false, 0)
